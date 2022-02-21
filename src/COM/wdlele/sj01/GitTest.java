3ea37bea90fed671ff5e4aec2f46320bdc39ec2e
wdlele <9815196@qq.com>
2022-02-21 10:04:30 +0800

    v01.01

---
+++ src/COM/wdlele/sj01/GitTest.java
@@ -0,0 +1,12 @@
+package COM.wdlele.sj01;
+
+/**
+ * @author wdlele
+ * @create 2022-02-21-9:54
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
