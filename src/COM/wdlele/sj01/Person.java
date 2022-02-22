1ff6412122fbd85e9f8b417c8d1b55808aa81444
wdlele <9815196@qq.com>
2022-02-22 16:08:31 +0800

    v01.04

---
+++ src/COM/wdlele/sj01/Person.java
@@ -0,0 +1,67 @@
+package COM.wdlele.sj01;
+
+import java.util.Objects;
+
+/**
+ * @author wdlele
+ * @create 2022-02-22-9:27
+ */
+public class Person {
+    private String name;
+    public  int age;
+
+    public Person() {
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    private Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public void show(){
+        System.out.println("你好，People 是个人");
+    }
+    private String showNation(String nation){
+        System.out.println("我的国籍是：" + nation);
+        return nation;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age && Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+}
